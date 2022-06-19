bd094502e626b3304d5a55ac5189c88321f09eec
Piotr "Bogdan" Chudzik-Lipka <p.chudziklipka0@gmail.com>
2022-06-19 21:43:15 +0200

    Updated conditions in main page

--- src/main/java/com/example/edp_project/HelloController.java
+++ src/main/java/com/example/edp_project/HelloController.java
@@ -1,20 +1,40 @@
 package com.example.edp_project;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-
 public class HelloController {
+    @FXML
     public TextField imie2TextField;
+    @FXML
     public TextField imie1TextField;
+    @FXML
     public Label wynikText;
+    @FXML
     public CheckBox pewnyCheckBox;
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public void onSprawdzButtonClick(ActionEvent actionEvent) {
+        wynikText.setText(null);
+        if(imie2TextField.getText().contains("Soplica")) {
+            wynikText.setText(("Lepszej połówki nie znajdziesz <3"));
+            return;
+        }
+        if(imie1TextField.getText().isEmpty() || imie2TextField.getText().isEmpty()) {
+            wynikText.setText("Oba pola imion muszą być wypełnione");
+            return;
+        }
+        if(!pewnyCheckBox.isSelected()){
+            wynikText.setText(("Zaznacz pole pewności"));
+            return;
+        }
+
+
+
+
     }
 }
