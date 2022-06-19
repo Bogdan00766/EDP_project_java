58d7e676453d3daf5b65cd12e26b533f9859d770
Piotr "Bogdan" Chudzik-Lipka <p.chudziklipka0@gmail.com>
2022-06-19 21:30:35 +0200

    Created mainPage

--- src/main/java/com/example/edp_project/HelloController.java
+++ src/main/java/com/example/edp_project/HelloController.java
@@ -1,9 +1,15 @@
 package com.example.edp_project;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+    public TextField imie2TextField;
+    public TextField imie1TextField;
+    public Label wynikText;
+    public CheckBox pewnyCheckBox;
     @FXML
     private Label welcomeText;
 
