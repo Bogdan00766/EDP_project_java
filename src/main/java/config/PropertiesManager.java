ec352045cd83891bc2c648cc1d6c712d0d7b382b
Piotr "Bogdan" Chudzik-Lipka <p.chudziklipka0@gmail.com>
2022-06-23 15:02:23 +0200

    Implmented Properties

---
+++ src/main/java/config/PropertiesManager.java
@@ -0,0 +1,48 @@
+package config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesManager {
+    private static final String PROPERTIES_FILE = "app.properties";
+    private Properties props = null;
+    private static PropertiesManager instance;
+
+    public PropertiesManager(){
+        loadProperties(PROPERTIES_FILE);
+    }
+
+    public static PropertiesManager getInstance(){
+        if(instance == null){
+            instance = new PropertiesManager();
+        }
+        return instance;
+    }
+    private void loadProperties(String filename) {
+        props = new Properties();
+        InputStream instream = null;
+
+        ClassLoader loader = this.getClass().getClassLoader();
+        instream = loader.getResourceAsStream(filename);
+        if(instream == null){
+            System.err.println("Unable to open propeties file " + filename);
+            return;
+        }
+
+        try{
+            props.load(instream);
+        }
+        catch (IOException e){
+            System.err.println("Error reading properties file " + filename);
+            System.err.println(e.getMessage());
+        }
+
+        try{
+            instream.close();
+        } catch (IOException ioe){};
+    }
+
+
+
+}
